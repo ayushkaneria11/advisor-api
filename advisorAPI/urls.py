7831923bcd8d67bd464a5746e5d96d61c50864f6
ayushkaneria11 <kaneriaayush@gmail.com>
2021-05-12 22:38:13 +0530

    bug fix

--- advisorAPI/urls.py
+++ advisorAPI/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, include
 from .views import addAdvisor, registerUser, loginUser, getAdvisorList, bookAdvisor, getBookedCalls
 
 urlpatterns = [
-    path('advisor/', addAdvisor),
-    path('user/register/', registerUser),
-    path('user/login/', loginUser),
-    path('user/<int:user_id>/advisor/', getAdvisorList),
-    path('user/<int:user_id>/advisor/<int:advisor_id>/', bookAdvisor),
-    path('user/<int:user_id>/advisor/booking/', getBookedCalls),
+    path('admin/advisor', addAdvisor),
+    path('user/register', registerUser),
+    path('user/login', loginUser),
+    path('user/<int:user_id>/advisor', getAdvisorList),
+    path('user/<int:user_id>/advisor/<int:advisor_id>', bookAdvisor),
+    path('user/<int:user_id>/advisor/booking', getBookedCalls),
     
 ]
